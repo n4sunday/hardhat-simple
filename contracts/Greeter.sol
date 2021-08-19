92dbdd9d4e65ba6c4610440a00a67806a16825b0
Sunday <nsoooooooooosn@gmail.com>
2021-08-20 04:12:33 +0700

    simple contracts and simeple test get string

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -3,20 +3,31 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract Greeter {
-    string private greeting;
+contract MyContract {
+    string private name;
+    string private fullname;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    constructor(string memory _name) {
+        name = _name;
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
+    function changeName(string memory _name) public {
+        name = _name;
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    function getName() public view returns (string memory) {
+        return name;
+    }
+
+    function changeFullname(string memory _fullname) public {
+        fullname = _fullname;
+    }
+
+    function getFullname() public view returns (string memory) {
+        return fullname;
+    }
+
+    function getNameProfile() public view returns (string memory) {
+        return string(abi.encodePacked(fullname, " ", name));
     }
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,34 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("MyContract", () => {
+  it("should return its name", async () => {
+    const MyContract = await ethers.getContractFactory("MyContract");
+    const myContract = await MyContract.deploy("My Contract");
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    await myContract.deployed();
+    expect(await myContract.getName()).to.equal("My Contract");
   });
+  it("should change its name when requested", async () => {
+    const MyContract = await ethers.getContractFactory("MyContract");
+    const myContract = await MyContract.deploy("My Contract");
+
+    await myContract.changeName("Sunday");
+    expect(await myContract.getName()).to.equal("Sunday");
+  });
+  it("should change its fullname when requested", async () => {
+    const MyContract = await ethers.getContractFactory("MyContract");
+    const myContract = await MyContract.deploy("My Contract");
+
+    await myContract.changeFullname("Nattapon");
+    expect(await myContract.getFullname()).to.equal("Nattapon")
+  })
+  it("should change its fullname and name when requested", async () => {
+    const MyContract = await ethers.getContractFactory("MyContract");
+    const myContract = await MyContract.deploy("My Contract");
+
+    await myContract.changeFullname("Nattapon");
+    await myContract.changeName("Sun");
+    expect(await myContract.getNameProfile()).to.equal("Nattapon Sun")
+  })
 });
